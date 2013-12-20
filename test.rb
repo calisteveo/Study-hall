48c0ddc1233411e49b211c0f408306e93c31f43f
calisteveo <soglesbee1@gmail.com>
2013-12-19 19:44:23 -0800

    added test.

---
+++ test.rb
@@ -0,0 +1,12 @@
+#saving my friend's name
+
+puts "Hello, what is your name?"
+
+name = gets.chomp
+
+# saving the number
+puts "How many prework assignments have you completed?"
+
+number = gets.chomp
+
+puts "Hello, #{name}. I'm Steve. Nice to meet you. I see you have completed #{number} assignments. I have just completed 1 assignment in my free time, I enjo
